1b8b0d5efe48260fa7267a65596c169e84fd37e4
suifengLuo <772396555@qq.com>
2014-11-27 20:11:28 +0800

    0.5

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-export GOPATH :=F:\working\gopro\fund
-export PATH := ${PATH}:${GOPATH}\bin
-export GOBIN := ${GOPATH}\bin
+export GOPATH :=/Users/suifengluo/Documents/gospace/fund
+export PATH := ${PATH}:${GOPATH}/bin
+export GOBIN := ${GOPATH}/bin
 main:
 	go run main.go
 build:
@@ -8,5 +8,17 @@ build:
 images:
 	docker build -t fund .
 run:
-	docker run -it -v F:\working\gopro\fund:/gopath --rm fund
+	docker run -it -v /Users/suifengluo/Documents/gospace/fund:/gopath --rm fund
 docker: images run
+
+dependence:
+	go get github.com/PuerkitoBio/goquery
+
+
+ssh:
+	ssh -i cloud.key ec2_user@<instance_ip>
+
+get:
+	go get github.com/go-sql-driver/mysql
+	go get github.com/go-xorm/xorm
+
--- main.go
+++ main.go
@@ -1,7 +1,96 @@
 package main
+
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
 )
+
+type FundInfo struct {
+	Code  uint32 `xorm:"pk"`
+	Name  string
+	Style string
+}
+
+type Worth struct {
+	Id                int64
+	Date              string //time.time
+	RealWorth         float32
+	AccumulativeWorth float32
+	FundCode          uint32 `xorm:"index"`
+}
+
+func NewFundInfo(code uint32, name string, style string) (obj *FundInfo) {
+	obj = new(FundInfo)
+	obj.Code = code
+	obj.Name = name
+	obj.Style = style
+	return
+}
+
+func NewWorth(date string, realWorth float32, accWorth float32, code uint32) (obj *Worth) {
+	obj = new(Worth)
+	obj.Date = date
+	obj.RealWorth = realWorth
+	obj.AccumulativeWorth = accWorth
+	obj.FundCode = code
+	return
+}
+
+func mysqlEngine() (*xorm.Engine, error) {
+
+	conn := "test:1234@/myData?charset=utf8"
+
+	return xorm.NewEngine("mysql", conn)
+}
+
+func sync(engine *xorm.Engine) error {
+	return engine.Sync(&FundInfo{}, &Worth{})
+}
+
 func main() {
-	fmt.Println("project is ready!!!")
+
+	orm, err := mysqlEngine()
+	defer orm.Close()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+		return
+	}
+	err = sync(orm)
+	if err != nil {
+		panic(err)
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("...")
+	resp, err := http.Get("http://www.howbuy.com/fund/fundranking/")
+
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	// b, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("11111")
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("22222")
+	doc.Find(".result_list table tbody tr").Each(func(i int, s *goquery.Selection) {
+
+		if i > 2 {
+			fmt.Println(i)
+			return
+		}
+		s.Find("td").Each(func(k int, sel *goquery.Selection) {
+			text := sel.Text()
+			fmt.Println(text)
+		})
+
+	})
+
 }
