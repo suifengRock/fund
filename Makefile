fd88b2579cb68cb3eefc4fb6fb0868f39d98cca0
suifengLuo <772396555@qq.com>
2014-11-28 15:39:35 +0800

    add the base info

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-export GOPATH := F:\working\fund
+export GOPATH := $(shell pwd)
 export PATH := ${PATH}:${GOPATH}\bin
 export GOBIN := ${GOPATH}\bin
-main:
+DOCKER_RUN_GO := fig run --rm goapp
+FIG_VERSION := $(shell fig --version 2>/dev/null)
+
+main:clear-pkg
 	go run main.go
 build:
 	go install main.go
@@ -14,13 +17,26 @@ docker: images run
 dependence:
 	go get github.com/go-sql-driver/mysql
 	go get github.com/go-xorm/xorm
+	go get github.com/qiniu/iconv
 	go get github.com/PuerkitoBio/goquery
 
+fig: 
+ifdef FIG_VERSION
+	@echo "Found fig version $(FIG_VERSION)"
+else
+	@echo fig Not found try to install it
+	curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
+endif
 
-ssh:
-	ssh -i cloud.key ec2_user@<instance_ip>
 
+clear-pkg:
+	rm -rf pkg/
 
-utf:
-	go get -u github.com/djimenez/iconv-go
+shell: fig
+	$(DOCKER_RUN_GO) bash
 
+mysqlc: fig
+	$(DOCKER_RUN_GO) go run mysql.go
+
+mainc: fig
+	$(DOCKER_RUN_GO) go run main.go
--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
-	"strings"
+	"github.com/qiniu/iconv"
 )
 
 type FundInfo struct {
 	Id    int64
-	Code  string
-	Name  string
+	Code  string `xorm:"index unique"`
+	Name  string `xorm:"not null"`
 	Style string
 }
 
 type Worth struct {
 	Id                int64
-	Date              string //time.time
+	Date              string
 	RealWorth         float32
 	AccumulativeWorth float32
 	FundCode          string `xorm:"index"`
@@ -43,7 +44,19 @@ func NewWorth(date string, realWorth float32, accWorth float32, code string) (ob
 
 func mysqlEngine() (*xorm.Engine, error) {
 
-	conn := "root:123456@/test?charset=utf8"
+	addr := os.Getenv("DB_PORT_3306_TCP_ADDR")
+	port := os.Getenv("DB_PORT_3306_TCP_PORT")
+	proto := os.Getenv("DB_PORT_3306_TCP_PROTO")
+	user := os.Getenv("DB_ENV_MYSQL_USER")
+	password := os.Getenv("DB_ENV_MYSQL_PASSWORD")
+	database := os.Getenv("DB_ENV_MYSQL_DATABASE")
+
+	conn := "test:1234@/myData?charset=utf8"
+
+	if addr != "" {
+		conn = fmt.Sprintf("%v:%v@%v(%v:%v)/%v?charset=utf8", user, password, proto, addr, port, database)
+		fmt.Println("the connection is " + conn)
+	}
 
 	return xorm.NewEngine("mysql", conn)
 }
@@ -54,93 +67,59 @@ func sync(engine *xorm.Engine) error {
 
 func main() {
 
-	// orm, err := mysqlEngine()
-	// defer orm.Close()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	panic(err)
-	// 	return
-	// }
-	// err = sync(orm)
-	// if err != nil {
-	// 	panic(err)
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	fmt.Println("...")
-
-	// tianFund := "http://fund.eastmoney.com/fund.html"
-	haoFund := "http://www.howbuy.com/board/"
-	// resp, err := http.Get(tianFund)
-	// defer res.Body.Close()
-	// if err != nil {
-	// 	fmt.Println("error: ", err)
-	// }
-	// b, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("11111")
-	doc, err := goquery.NewDocument(haoFund)
+	orm, err := mysqlEngine()
+	defer orm.Close()
 	if err != nil {
+		fmt.Println(err)
+		panic(err)
+		return
+	}
+	err = sync(orm)
+	if err != nil {
+		panic(err)
 		fmt.Println(err)
 		return
 	}
-	dataList := doc.Find(".result_list")
-	dataList.Find("textarea").Each(func(num int, sl *goquery.Selection) {
-		if num > 0 {
+	baseData, _ := getBaseFundInfoByTT()
+	orm.Insert(baseData)
+	fmt.Println("update......ok")
+
+}
+
+func getBaseFundInfoByTT() ([]*FundInfo, error) {
+
+	urlFund := "http://fund.eastmoney.com/fund.html"
+	doc, err := goquery.NewDocument(urlFund)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	cd, err := iconv.Open("utf-8", "gbk")
+	if err != nil {
+		fmt.Println("iconv.Open failed!")
+		return nil, err
+	}
+	defer cd.Close()
+	baseData := make([]*FundInfo, 0)
+	doc.Find("#oTable tbody tr").Each(func(i int, s *goquery.Selection) {
+		if i == 0 {
 			return
 		}
-		sl.RemoveAttr("attr")
-		textArea := sl.Text()
-		textList := strings.Split(textArea, "<tr>")
-		fmt.Println("len", len(textList))
-		textSli := strings.Split(textArea, textList[0])
-		length := len(textSli)
-		for tmp := 0; tmp < length; tmp += 2 {
-			val := textSli[tmp]
-			valList := strings.Split(val, "</tr>")
-			html := valList[0] + "</tr>"
-			html = strings.Replace(html, "\"", "'", -1)
-			fmt.Println(html)
-			dataList.Find("table tbody").AppendHtml("<tr><td width='4%'><input  type='checkbox' onclick='move(this);' value='161605'/></td><td width='5%'>100</td><td width='6%'><a target='_blank' href='/fund/161605/'>161605</a></td><td  class='tdl'><a href='/fund/161605/'>融通蓝筹成长混合</a></td><td  class='tdr'>1.2260</td><td  class='tdr'>2.5590</td><td  class='tdr'>1.2070</td><td  class='tdr'>2.5400</td><td  class='tdr'><span class='cRed'>0.0190</span></td><td  class='tdr'><span class='cRed'>1.57%</span></td><td  class='operate'><a href='https://trade.ehowbuy.com/trade/subs.htm?method=apply&fundCode=161605' class='sg'  title='申购'></a><a href='https://trade.ehowbuy.com/savingplan/index.htm?method=apply&fundCode=161605' class='dt' title='定投'></a><a href='javascript:void(0)' url_add='http://www.howbuy.com/fund/fundtool/addAttentionFund.htm?fundCode=161605' url_del='http://www.howbuy.com/fund/fundtool/removefund.htm?fundCode=161605' class='zx' title='自选' target='_self'></a></td></tr>")
-		}
-
-	})
-
-	dataList.Find("table tbody tr").Each(func(i int, s *goquery.Selection) {
 		code := ""
 		name := ""
 		s.Find("td").Each(func(key int, sel *goquery.Selection) {
 
-			if key == 2 {
+			if key == 3 {
 				code = sel.Text()
 			}
-			if key == 3 {
-				name = sel.Text()
+			if key == 4 {
+				name = sel.Find("nobr a").First().Text()
 			}
 		})
-		fmt.Println(code, " ", name)
+		obj := NewFundInfo(code, cd.ConvString(name), "")
+		baseData = append(baseData, obj)
 	})
 
-	// fmt.Println(dataList.Find("table tbody tr").Text())
-
-	// doc.Find(".reviews-wrap article .review-rhs").Each(func(i int, s *goquery.Selection) {
-	// 	band := s.Find("h3").Text()
-	// 	title := s.Find("i").Text()
-	// 	fmt.Printf("Review %d: %s - %s\n", i, band, title)
-	// })
-
-	// html := doc.Find(".tableDiv").Text()
-	// fmt.Print(html)
-	// doc.Find("tableDiv table tbody tr").Each(func(i int, s *goquery.Selection) {
-
-	// 	if i > 2 {
-	// 		fmt.Println(i)
-	// 		return
-	// 	}
-	// 	s.Find("td").Each(func(k int, sel *goquery.Selection) {
-	// 		text := sel.Text()
-	// 		fmt.Println(text)
-	// 	})
-
-	// })
-
+	return baseData, nil
 }
